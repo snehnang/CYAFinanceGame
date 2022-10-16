4d68455bb6f45ee6fdb151459493b673496c229a
Benita Abraham <94129178+beniabra@users.noreply.github.com>
2022-10-16 08:51:10 -0400

    completed the branches

--- CYAFinanceGame.py
+++ CYAFinanceGame.py
@@ -16,6 +16,12 @@ def getInput(choices):
                 print('Please enter a number between 1 and ' + str(choices))
 
 def playAgain():
+    global account
+    change = account - 50
+    if(change > 0):
+        print('You made ' + str(change) + ' dollars today.')
+    else:
+        print('You lost ' + str(abs(change)) + ' dollars today.')
     print('Would you like to play again?')
     print('1. Yes')
     print('2. No')
@@ -24,11 +30,14 @@ def playAgain():
 
     if(choice == 1):
         start()
+    else:
+        print('Thanks for playing!')
 
 def start():
+    print('Welcome to Choose Your Own Adventure Finance Game!\n')
     print('You are a broke college student who just started working part time at a movie theater for $15 an hour. \nThis is a day in your life. You must make wise decisions and manage your finances well.')
     updateAccount(0);
-    print('\nYou just woke up at 8:00 and you have a class at 8:30 but you are really tired.\n')
+    print('\nYou just woke up at 8:00 and you have a class at 8:30 but you are really tired.')
     print('1. Get a $7 coffee and be 10 minutes late to class.')
     print('2. Go back to sleep.')
     print('3. Take the bus $3 and arrive on time.')
@@ -50,6 +59,7 @@ def start():
             updateAccount(-5)
         elif(choice == 3):
             updateAccount(0)
+            scam()
         
         atSchool()
         
@@ -68,11 +78,12 @@ def start():
             updateAccount(-6)
         elif(choice == 3):
             updateAccount(0)
+            scam()
             
         print('After having brunch, you get a call to take some surveys for $1 each. How many surveys would you like to take?')
-        print('1. 20 surveys for $20')
-        print('2. 10 surveys for $10')
-        print('3. 0 surveys for $0')
+        print('1. Get $20 for 20 surveys')
+        print('2. Get $10 for 10 surveys')
+        print('3. Get $0 for 0 surveys')
         
         choice = getInput(3)
         
@@ -98,12 +109,12 @@ def start():
         
         atSchool()
     
-    # go to work // scam
+    work()
 
 def updateAccount(x):
     global account
     account = account + x
-    print('Bank Account: $' + str(account))
+    print('Bank Account: $' + str(account) + "\n")
 
 def atSchool():
     print('At school, your teacher mentions an opportunity to develop a simple app for him in Python and will pay $30.')
@@ -117,7 +128,7 @@ def atSchool():
         print('As you are coding away, you run into some technical difficulties.')
         print('1. You pay $15 for a Chegg subscription to solve your problems.')
         print('2. You get your friend to solve your problems for $9.')
-        print('3. You stay up till 2am and figure it out yourself.')
+        print('3. You stick with it until you figure it out yourself.')
         
         choice = getInput(3)
         
@@ -130,7 +141,30 @@ def atSchool():
         
     elif(choice == 2):
         updateAccount(0)
-
+        
+def scam():
+    print('The concert ticket you tried to purchase last week turned out to be a scam. You lost $20.')
+    print('Press 1 to continue...')
+    
+    
+    choice = getInput(1)
+    
+    if(choice == 1):
+        updateAccount(-20)
+    
+    
+def work():
+    print('It\'s 6pm and your 4-hour shift at work is about to start. Do you go or not?')
+    print('1. Call out sick.')
+    print('2. Drag yourself to work.')
+    
+    choice = getInput(2)
+    
+    if(choice == 1):
+            updateAccount(0)
+    elif(choice == 2):
+            updateAccount(24)
+    
+    playAgain()
 
 start()
-
